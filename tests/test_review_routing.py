d42e5e0b7912701f486f1b74f9fe1711e592851c
Maxim Shcherbakov <maksimkachentr228@gmail.com>
2026-08-14 01:48:28 +0700

    test: cover shell-launched gradle wrappers

--- tests/test_review_routing.py
+++ tests/test_review_routing.py
@@ -1,4 +1,4 @@
-from distill_hook.router import select_filter
+from distill_hook.router import normalize_command, select_filter
 
 
 def test_test_family_matches_only_the_invoked_command():
@@ -9,3 +9,19 @@ def test_test_family_matches_only_the_invoked_command():
     assert select_filter("python -m " + test_runner + " -q").name == "tests"
     assert select_filter(search_tool + " " + test_runner).name == "search"
     assert select_filter(version_control + " diff -- " + test_runner + ".ini").name == "git_diff"
+
+
+def test_simple_shell_launcher_normalizes_local_gradle_wrapper():
+    assert normalize_command("sh ./gradlew test") == "gradlew test"
+    assert (
+        normalize_command("bash ./gradlew connectedDebugAndroidTest")
+        == "gradlew connectedDebugAndroidTest"
+    )
+    assert normalize_command("/bin/sh ./gradlew test") == "gradlew test"
+    assert select_filter("sh ./gradlew test").name == "lint_build"
+    assert select_filter("bash ./gradlew connectedDebugAndroidTest").name == "lint_build"
+
+
+def test_shell_program_flags_are_not_normalized_as_simple_launchers():
+    assert normalize_command("sh -c ./gradlew test") == "sh -c ./gradlew test"
+    assert normalize_command("bash -lc ./gradlew test") == "bash -lc ./gradlew test"
